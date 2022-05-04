b814e192ace738714feea656fc6bf2fb2c1b0133
shimomura-hiroki <69880795+shimomura-hiroki@users.noreply.github.com>
2022-05-04 15:40:32 +0900

    reviewCreate

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+/* lib */
+import { getShops } from "./src/lib/firebase";
+/* types */
+import { Shop } from "./src/types/shop";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [shops, setShops] = useState<Shop[]>([]);
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const shops = await getShops()
+    setShops(shops);
+  };
+
+  const shopItems = shops.map((shop, index) => (
+    <View style={{margin: 10}} key={index.toString()}>
+      <Text>{shop.name}</Text>
+      <Text>{shop.place}</Text>
     </View>
+  ));
+
+
+  return (
+    <View style={styles.container}>{shopItems}</View>
   );
 }
 
---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+/* type */
+import { Shop } from "../types/shop";
+import Constants from "expo-constants"
+
+if (firebase.apps.length === 0) {
+    firebase.initializeApp(Constants.manifest!.extra!.firebase);
+};
+
+export const getShops = async () => {
+const snapshot = await firebase.firestore().collection("shops").get();
+const shops = snapshot.docs.map((doc: { data: () => any; }) => doc.data() as Shop);
+return shops;
+};
